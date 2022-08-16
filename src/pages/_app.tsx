6f32549fb96801d45da19abf7f754663ec56a7f1
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-16 08:43:12 -0300

    feat: config theme colors using chakra-ui/react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { AppProps } from "next/app";
 
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    // resetCSS - Limpa todas as configurações nativas do HTML, ex: padding no body, margin e etc.
+    // resetCSS -> default da biblioteca já é true, pode ser omitido
+    <ChakraProvider theme={theme} resetCSS> 
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
