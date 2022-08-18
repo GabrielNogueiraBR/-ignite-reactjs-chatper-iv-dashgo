bb71b853aa0b5a84f306825614badd1f6e507b4e
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-18 10:04:34 -0300

    chore: creating components to handle with input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 
-import {
-  Button,
-  Flex,
-  Input,
-  Stack,
-  FormLabel,
-  FormControl,
-} from "@chakra-ui/react";
+import { Button, Flex, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 export default function Home() {
   return (
@@ -22,34 +16,8 @@ export default function Home() {
         flexDirection="column"
       >
         <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <Input
-              name="email"
-              type="email"
-              focusBorderColor="pink.500"
-              backgroundColor="gray.900"
-              variant="filled"
-              size="lg"
-              _hover={{
-                backgroundColor: "gray.900",
-              }}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input
-              name="password"
-              type="password"
-              focusBorderColor="pink.500"
-              backgroundColor="gray.900"
-              variant="filled"
-              size="lg"
-              _hover={{
-                backgroundColor: "gray.900",
-              }}
-            />
-          </FormControl>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
         </Stack>
 
         <Button type="submit" marginTop={6} colorScheme="pink" size={"lg"}>
