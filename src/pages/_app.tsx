83654bded86297da15888cddd47c2cf1468387af
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-26 09:35:00 -0300

    feat: adding Drawer Sidebar using chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import { AppProps } from "next/app";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // resetCSS - Limpa todas as configurações nativas do HTML, ex: padding no body, margin e etc.
     // resetCSS -> default da biblioteca já é true, pode ser omitido
-    <ChakraProvider theme={theme} resetCSS> 
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme} resetCSS>
+      <SidebarDrawerProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
   );
 }
