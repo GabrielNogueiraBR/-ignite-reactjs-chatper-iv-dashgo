2c5757c057b4d9c0d5c1772969ba10022cfd68d4
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-01-04 19:43:07 -0300

    chore: install React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { AppProps } from "next/app";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
@@ -11,14 +12,18 @@ if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // resetCSS - Limpa todas as configurações nativas do HTML, ex: padding no body, margin e etc.
     // resetCSS -> default da biblioteca já é true, pode ser omitido
     <ChakraProvider theme={theme} resetCSS>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
