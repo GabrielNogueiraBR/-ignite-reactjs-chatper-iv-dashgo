7407fb3ec3785963003098c8ae6b7a5950b4d267
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-01-04 22:13:23 -0300

    fix: date property

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -27,10 +27,9 @@ export const getUsers = async (page: number): Promise<GetUsersResponse> => {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+      createdAt: new Date(user.created_at).toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "long",
-
         year: "numeric",
       }),
     };
